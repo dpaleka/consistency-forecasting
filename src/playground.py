dab7078e560600249c5c03d47464fd85bfe68512
abhimanyu <abhimanyupallavisudhir@gmail.com>
2024-02-28 19:07:27 +0000

    refactor

--- src/playground.py
+++ src/playground.py
@@ -1,7 +1,6 @@
 #%%
-from static_checks.negation import negation_template, negate_simple, negation_checker
-from common.models import *
-from static_checks.checkers import *
+from static_checks.Negation import *
+from common.forecasters import *
 
 # Instantiate some base questions
 BASE_QS = [
@@ -9,30 +8,11 @@ BASE_QS = [
     "What is the probability that Ebola will be eradicated by 2030?",
 ]
 
-# instantiate the negation checks
-negation_check_qtuples = [
-    negation_template["tuple_generator"](q)
-    for q in BASE_QS
-]
-
-# Now call some LLM method on those qs to get answers
-answers = [[0.5, 0.5], [0.6, 0.4]]
-pass
-
-# Now check the answers
-violations = [
-    negation_template["violation_scorer"](*ans)
-    for ans in answers
-]
-passed = [
-    negation_template["probs_checker"](*ans)
-    for ans in answers
-]
-
-t=NegationChecker()
-u = t.instantiate_and_elicit_and_check(gpt4caster, BASE_QS[1])
+u = NegationChecker().instantiate_and_elicit_and_check(gpt4caster, BASE_QS[1])
 print(u)
 
 # Now we can make a report
 import matplotlib.pyplot as plt
 pass
+
+# %%
--- src/static_checks/negation.py
+++ src/static_checks/negation.py
@@ -1,93 +1,15 @@
-"""
-Formal constraint:
-“desc”: “A -> B”
-“probs_checker”: def check_condition(ans_A : Prob, ans_B : Prob):
-return ans_A < ans_B
-“probs_violation”: def viol_score(ans_A : Prob, ans_B):
-return max(0, ans_A - ans_B)
-
-def negate_simple(ques: str) -> str:
-cons_check = {“tup”:  (“Will Dems win election in 2024”, make_negative_q(ques_str)), “desc”: ...,  “checker”: NegationChecker, )
-cons_check_template = {“tuple_generator”: Callable = make_negative_q, “desc”: ...,  “output_parser”: Callable : str -> float, “probs_checker”: NegationChecker, )
-
-make a class and subclass it for the negation thing
-"""
-
-# Path: static_checks/negation.py
-
-from abc import ABC, abstractmethod
-from typing import Type, Dict, NamedTuple, Callable
-# path imports
+# Path: static_checks/BaseChecker.py
 import sys
+from common.llm_utils import query_api_chat_sync
+from common.forecasters import SentencesTemplate, ProbsTemplate
+from static_checks.Base import BaseChecker
 
 sys.path.append("..")
 
-from instantiators.negation import negate_simple
-from common.llm_utils import query_api_chat_sync
 
-class Prob(float):
-    def __new__(cls, value):
-        if not (0.0 <= value <= 1.0):
-            raise ValueError("Probability must be between 0 and 1.")
-        return super(Prob, cls).__new__(cls, value)
-
-Forecaster = Callable[[str], Prob]
-SentencesTemplate = Dict[str, str]
-ProbsTemplate = Dict[str, Prob]
-
-def elicit(forecaster : Forecaster, sentences : SentencesTemplate) -> ProbsTemplate:
-    return {k : forecaster(v) for k, v in sentences.items()}
-
-def gpt4caster (sentence : str) -> Prob:
-    messages = [
-        {
-            "role" : "system",
-            "content" : "You are an informed and well-calibrated forecaster. I need you to give me your best probability estimate for the following sentence or question resolving YES. Your answer should be a float between 0 and 1, with nothing else in your response."
-        },
-        {
-            "role" : "user",
-            "content" : sentence
-        }
-    ]
-    response = query_api_chat_sync(
-        model="gpt-4-1106-preview",
-        messages=messages,
-        temperature=0.0,
-    )
-    return float(response)
-
-class ConsistencyChecker(ABC):
-    
-    def __init__(self, tolerance = 0.1):
-        self.tolerance = tolerance
-
-    @abstractmethod
-    def instantiate(self, *base_sentences : str) -> SentencesTemplate:
-        pass
-    
-    @abstractmethod
-    def violation(self, answers : ProbsTemplate) -> float:
-        pass
-
-    def check(self, answers : ProbsTemplate) -> bool:
-        return self.violation(answers) < self.tolerance
-
-    def instantiate_and_elicit(self, forecaster : Callable[[str], Prob], *base_sentences : str) -> ProbsTemplate:
-        return elicit(forecaster, self.instantiate(*base_sentences))
-    
-    def instantiate_and_elicit_and_violation(self, forecaster : Callable[[str], Prob], *base_sentences : str) -> float:
-        return self.violation(self.instantiate_and_elicit(forecaster, *base_sentences))
-    
-    def instantiate_and_elicit_and_check(self, forecaster : Callable[[str], Prob], *base_sentences : str) -> float:
-        return self.check(self.instantiate_and_elicit(forecaster, *base_sentences))
-
-
-class NegationChecker(ConsistencyChecker):
-    
-    def __init__(self, tolerance=0.1):
-        super().__init__(tolerance)
-    
-    def instantiate(self, base_sentence: str) -> Dict[str, str]:
+class NegationChecker(BaseChecker):
+        
+    def instantiate(self, base_sentence: str) -> SentencesTemplate:
         messages = [
             {
                 "role": "system",
@@ -108,35 +30,3 @@ class NegationChecker(ConsistencyChecker):
 
     def violation(self, answers: ProbsTemplate) -> float:
         return abs(answers["P"] + answers["notP"] - 1)
-
-
-
-########### old code
-
-def negation_violation(ans_A: Prob, ans_B: Prob) -> float:
-    """
-    Returns the difference between the two.
-    """
-    return abs(ans_A + ans_B - 1)
-
-
-def negation_checker(ans_A: Prob, ans_B: Prob, tolerance: float = 0.1) -> bool:
-    """
-    Checks if the two sum up to 1.
-    """
-    return negation_violation(ans_A, ans_B) < tolerance
-
-
-def instantiate(base_q: str) -> tuple[str]:
-    """
-    Instantiates a negation constraint.
-    """
-    return (base_q, negate_simple(base_q))
-
-
-negation_template = {
-    "tuple_generator": instantiate,
-    "desc": "A -> B",
-    "violation_scorer": negation_violation,
-    "probs_checker": negation_checker,
-}
