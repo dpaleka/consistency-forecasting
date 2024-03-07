3d6a3239bcc7e4607b05300a3a5d21d08459f078
Gusanidas <33495733+Gusanidas@users.noreply.github.com>
2024-03-07 23:08:16 +0100

    some more forecasters (#4)

--- src/forecasters/__init__.py
+++ src/forecasters/__init__.py
@@ -1,4 +1,6 @@
 from .basic_forecaster import BasicForecaster
 from .forecaster import Forecaster, Prob, SentencesTemplate, ProbsTemplate 
+from .consistency_forecaster import ConsistencyForecaster
+from .reasoning_forecaster import ReasoningForecaster
 
 
