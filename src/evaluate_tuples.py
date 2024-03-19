1aa9a42090864cd79fd55256b42dbc8cf3f1e5f7
abhimanyu <abhimanyupallavisudhir@gmail.com>
2024-03-19 16:41:22 +0000

    bugfix for evaluate_tuples

--- src/evaluate_tuples.py
+++ src/evaluate_tuples.py
@@ -4,9 +4,7 @@ from static_checks import *
 basic_forecaster = BasicForecaster()
 
 negation_checker = NegChecker() # or NegationChecker(path = "src/data/NegationChecker.jsonl")
-paraphrasal_checker = ParaphrasalChecker()
-butnot_checker = ButNotChecker()
 
-#negation_checker.test(basic_forecaster, model = "gpt-3.5-turbo")
-butnot_checker.test(basic_forecaster)
-paraphrasal_checker.test(basic_forecaster)
+negation_checker.test(basic_forecaster, model = "gpt-3.5-turbo")
+# butnot_checker.test(basic_forecaster)
+# paraphrasal_checker.test(basic_forecaster)
