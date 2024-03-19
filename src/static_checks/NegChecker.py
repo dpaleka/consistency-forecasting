8c9af6de70b1a454272cd295ed8c149e054f3ec8
abhimanyu <abhimanyupallavisudhir@gmail.com>
2024-03-19 15:50:22 +0000

    implemented it for NegChecker

--- src/static_checks/NegChecker.py
+++ src/static_checks/NegChecker.py
@@ -3,29 +3,59 @@ R(x1, x2)       :=  x2 == ¬x1
 S(f(x1), f(x2)) :=  f(x1) + f(x2) = 1
 """
 
-from common.llm_utils import answer_sync, answer
-from forecasters import SentencesTemplate, ProbsTemplate
+from common.llm_utils import answer_sync, answer, QandA
+from common.datatypes import *
 from .BaseChecker import BaseChecker
 
 
 class NegChecker(BaseChecker):
     preface = (
-        "You are a helpful assistant. I need you to negate the question provided. "
-        "This should be done by adding / removing the word 'not' whenever possible. "
-        "Demorgan's laws should be followed with and/or negation. It should return a "
-        "question. Avoid using the word won't.")
-    
+        "You are a helpful assistant. I will give you a forecasting question with Yes/No "
+        "answer, in the format: \n\n"
+        "TITLE: [Title of the question]\n"
+        "RESOLUTION DATE: [Date of resolution]\n"
+        "DETAILS: [Question body with details]\n\n"
+        "You should then give me the NEGATION of the question, i.e. the question that "
+        "would be answered YES if the original question would be answered NO, and vice "
+        "versa. You MUST give me your answer in the same format as specified below. "
+        "Demorgan's laws should be followed with and/or negation. Avoid using the word "
+        "'won't'."
+    )
+
+    examples = [
+        QandA(
+            question=(
+                "TITLE: Will the price of Bitcoin be above $100,000 on 1st January 2025?\n"
+                "RESOLUTION DATE: 1st January 2025\n"
+                "DETAILS: Resolves YES if the spot price of Bitcoin against USD is more than 100,000 "
+                "on 1st January 2025. Resolves NO otherwise."
+            ),
+            answer=(
+                "TITLE: Will the price of Bitcoin be less than or equal to $100,000 on 1st January 2025?\n"
+                "RESOLUTION DATE: 1st January 2025\n"
+                "DETAILS: Resolves YES if the spot price of Bitcoin against USD is less than or equal to "
+                "100,000 on 1st January 2025. Resolves NO otherwise."
+            ),
+        )
+    ]
+
     def __init__(self, tolerance=0.1):
         super().__init__(tolerance)
 
-    def instantiate_sync(self, base_sentence: str, **kwargs) -> SentencesTemplate:
-        response = answer_sync(prompt = base_sentence, preface = self.preface, **kwargs)
-        sentences = {"P": base_sentence, "notP": response}
+    def instantiate_sync(self, base_sentence: Sentence, **kwargs) -> SentencesTemplate:
+        prompt = self.stack(base_sentence)
+        response = answer_sync(
+            prompt=prompt, preface=self.preface, examples=self.examples, **kwargs
+        )
+        sentences = {"P": base_sentence, "notP": Sentence.from_str(response)}
         return sentences
 
-    async def instantiate(self, base_sentence: str, **kwargs) -> SentencesTemplate:
-        response = await answer(prompt = base_sentence, preface = self.preface, **kwargs)
-        sentences = {"P": base_sentence, "notP": response}
+    async def instantiate(self, base_sentence: Sentence, **kwargs) -> SentencesTemplate:
+        prompt = self.stack(base_sentence)
+        response = await answer(
+            prompt=prompt, preface=self.preface, examples=self.examples, **kwargs
+        )
+        sentences = {"P": base_sentence, "notP": Sentence.from_str(response)}
         return sentences
 
     def violation(self, answers: ProbsTemplate) -> float:
