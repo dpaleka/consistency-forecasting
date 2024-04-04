9a4fef06e0cb01a2896b556dc80bed25cf51f09e
abhimanyu <abhimanyupallavisudhir@gmail.com>
2024-04-05 04:21:47 +0530

    Checker.

--- src/static_checks/BaseChecker.py
+++ src/static_checks/BaseChecker.py
@@ -1,7 +1,7 @@
 # Path: static_checks/Base.py
 import jsonlines
 from abc import ABC, abstractmethod
-from typing import Type, Self
+from typing import Type, Self, Any
 from pydantic import BaseModel
 from common.utils import write_jsonl_async_from_str
 from common.llm_utils import parallelized_call
@@ -19,6 +19,9 @@ class MiniInstantiator(ABC):
     # class BaseSentenceFormat(BaseModel):
     #     pass
 
+    def __init__():
+        pass
+    
     @property
     @abstractmethod
     def BaseSentenceFormat(self) -> Type[BaseModel]:
@@ -104,68 +107,58 @@ class MiniInstantiator(ABC):
 
 class Trivial(MiniInstantiator):
         
-        class BaseSentenceFormat(BaseModel):
-            P: ForecastingQuestion
+    class BaseSentenceFormat(BaseModel):
+        P: ForecastingQuestion
+
+    def title_body_sync_(
+        self, base_sentences: "Self.BaseSentenceFormat", **kwargs
+    ) -> ForecastingQuestion_stripped:
+        return base_sentences.P.cast_simple()
     
-        async def title_body_(
-            self, base_sentences: "Self.BaseSentenceFormat", **kwargs
-        ) -> ForecastingQuestion_stripped:
-            return base_sentences.P.cast_simple()
+    async def title_body_(
+        self, base_sentences: "Self.BaseSentenceFormat", **kwargs
+    ) -> ForecastingQuestion_stripped:
+        return base_sentences.P.cast_simple()
+
+    def resolution_(
+        self, resolutions: dict[str, bool]
+    ) -> Optional[bool]:
+        return resolutions["P"]
+
+class Checker(ABC):
     
-        def resolution_(
-            self, resolutions: dict[str, bool]
-        ) -> Optional[bool]:
-            return resolutions["P"]
-
-class TupleInstantiator(ABC):
-    ...
-
-class BaseChecker(ABC):
-
-    @property
-    @abstractmethod
-    def BaseSentenceFormat(self) -> Type[BaseModel]:
-        pass
-
-    @property
-    @abstractmethod
-    def TupleFormat(self) -> Type[BaseModel]:
-        pass
-
     def __init__(self, tolerance=0.1, path=None):
         self.tolerance = tolerance
         if path is None:
             self.path = f"src/data/{self.__class__.__name__}.jsonl"
         else:
             self.path = path
-
+        
+    @property
+    @abstractmethod
+    def TupleFormat(self) -> Type[BaseModel]:
+        pass
+    
     @abstractmethod
     def instantiate_sync(
         self, base_sentences: dict[str, ForecastingQuestion], **kwargs
     ) -> "Self.TupleFormat":
         pass
-
+    
     @abstractmethod
     async def instantiate(
         self, base_sentences: dict[str, ForecastingQuestion], **kwargs
     ) -> "Self.TupleFormat":
         pass
-
-    @abstractmethod
-    def violation(self, answers: dict[str, Prob]) -> float:
-        pass
-
+    
     async def instantiate_and_write(
         self, base_sentences: dict[str, ForecastingQuestion], **kwargs
     ):
         result = await self.instantiate(base_sentences, **kwargs)
-
-        if kwargs.get("verbose", True):
-            print(f"Writing tuple to {self.path}: {result}")
         await write_jsonl_async_from_str(
             self.path, [result.model_dump_json()], append=True
         )
-
+    
     async def instantiate_and_write_many(
         self, base_sentencess: list[dict[str, ForecastingQuestion]], **kwargs
     ):
@@ -173,20 +166,24 @@ class BaseChecker(ABC):
             base_sentences, **kwargs
         )
         await parallelized_call(_instantiate_and_write, base_sentencess)
-
-    def check(self, answers: dict[str, Prob]) -> bool:
+    
+    @abstractmethod
+    def violation(self, answers: dict[str, Prob]) -> float:
+        pass
+    
+    def check(self, answers: dict[str, Any]) -> bool:
         return self.violation(answers) < self.tolerance
-
+    
     def elicit_and_violation(
         self, forecaster: Forecaster, sentences: "Self.TupleFormat", **kwargs
     ) -> float:
         return self.violation(forecaster.elicit(sentences, **kwargs))
-
+    
     def elicit_and_check(
         self, forecaster: Forecaster, sentences: "Self.TupleFormat", **kwargs
     ) -> bool:
         return self.check(forecaster.elicit(sentences, **kwargs))
-
+    
     def test(self, forecaster: Forecaster, **kwargs):
         for line in jsonlines.open(self.path):
             print("START")
@@ -203,3 +200,88 @@ class BaseChecker(ABC):
             print(f"Violation: {loss}")
             print(f"Check result: {res}")
             print("")
+    
+
+# class BaseChecker(ABC):
+
+#     @property
+#     @abstractmethod
+#     def BaseSentenceFormat(self) -> Type[BaseModel]:
+#         pass
+
+#     @property
+#     @abstractmethod
+#     def TupleFormat(self) -> Type[BaseModel]:
+#         pass
+
+#     def __init__(self, tolerance=0.1, path=None):
+#         self.tolerance = tolerance
+#         if path is None:
+#             self.path = f"src/data/{self.__class__.__name__}.jsonl"
+#         else:
+#             self.path = path
+
+#     @abstractmethod
+#     def instantiate_sync(
+#         self, base_sentences: dict[str, ForecastingQuestion], **kwargs
+#     ) -> "Self.TupleFormat":
+#         pass
+
+#     @abstractmethod
+#     async def instantiate(
+#         self, base_sentences: dict[str, ForecastingQuestion], **kwargs
+#     ) -> "Self.TupleFormat":
+#         pass
+
+#     @abstractmethod
+#     def violation(self, answers: dict[str, Prob]) -> float:
+#         pass
+
+#     async def instantiate_and_write(
+#         self, base_sentences: dict[str, ForecastingQuestion], **kwargs
+#     ):
+#         result = await self.instantiate(base_sentences, **kwargs)
+
+#         if kwargs.get("verbose", True):
+#             print(f"Writing tuple to {self.path}: {result}")
+#         await write_jsonl_async_from_str(
+#             self.path, [result.model_dump_json()], append=True
+#         )
+
+#     async def instantiate_and_write_many(
+#         self, base_sentencess: list[dict[str, ForecastingQuestion]], **kwargs
+#     ):
+#         _instantiate_and_write = lambda base_sentences: self.instantiate_and_write(
+#             base_sentences, **kwargs
+#         )
+#         await parallelized_call(_instantiate_and_write, base_sentencess)
+
+#     def check(self, answers: dict[str, Prob]) -> bool:
+#         return self.violation(answers) < self.tolerance
+
+#     def elicit_and_violation(
+#         self, forecaster: Forecaster, sentences: "Self.TupleFormat", **kwargs
+#     ) -> float:
+#         return self.violation(forecaster.elicit(sentences, **kwargs))
+
+#     def elicit_and_check(
+#         self, forecaster: Forecaster, sentences: "Self.TupleFormat", **kwargs
+#     ) -> bool:
+#         return self.check(forecaster.elicit(sentences, **kwargs))
+
+#     def test(self, forecaster: Forecaster, **kwargs):
+#         for line in jsonlines.open(self.path):
+#             print("START")
+#             print(f"line: {line}")
+#             line_obj = self.TupleFormat.model_validate(line)
+#             answers = forecaster.elicit(line_obj, **kwargs)
+#             print(answers)
+#             if not all(answers.values()):
+#                 print("ERROR: Some answers are None!")
+#                 continue
+#             loss = self.violation(answers)
+#             res_bool = self.check(answers)
+#             res = {True: "Passed", False: "Failed"}[res_bool]
+#             print(f"Violation: {loss}")
+#             print(f"Check result: {res}")
+#             print("")
