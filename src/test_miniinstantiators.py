e11bd67d1fd238e7e267a5196dfa57ad6e91d2c2
abhimanyu <abhimanyupallavisudhir@gmail.com>
2024-04-06 05:04:14 +0530

    something's broken, the pydantic models in the previously defined instantiators interfere with those in the new one -- I'll figure it out tomorrow

--- src/test_miniinstantiators.py
+++ src/test_miniinstantiators.py
@@ -1,9 +1,13 @@
-from static_checks.NegChecker import Trivial, Neg
+from static_checks.BaseChecker import Trivial, Neg, And, Or, Paraphrase
 from common.datatypes import *
 import asyncio
 
-minitrivial = Trivial()
-minineg = Neg()
+mini_trivial = Trivial()
+mini_neg = Neg()
+mini_and = And()
+mini_or = Or()
+mini_para = Paraphrase()
+
 
 base_question = ForecastingQuestion(
     title="Will Jimmy Neutron be US president in 2025?",
@@ -13,10 +17,20 @@ base_question = ForecastingQuestion(
     data_source="synthetic",
     url="https://jimmyneutron.com",
 )
+base_question2 = ForecastingQuestion(
+    title="Will I eat a blueberry tomorrow?",
+    body="Resolves YES if I eat a blueberry tomorrow. Resolves NO otherwise.",
+    resolution_date=datetime(2025, 12, 31),
+    question_type="binary",
+    data_source="synthetic",
+    url="https://jimmyneutron.com",
+)
+
 base_questions = [{"P" :base_question}]
+#base_questions = [{"P" :base_question, "Q": base_question2}]
 
 async def foo():
-    x = await minineg.instantiate(base_questions[0])
+    x = await mini_para.instantiate(base_questions[0])
     print(x)
 
 asyncio.run(foo())
