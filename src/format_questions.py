8a6ad99e3cfdec1a4556adc9ed7ec33fd6d153e2
Gusanidas <Alejandrogus792@gmail.com>
2024-04-16 18:21:21 +0200

    Add ForecastingQuestion Validation

--- src/format_questions.py
+++ src/format_questions.py
@@ -7,33 +7,67 @@ from question_generators import question_formater
 from common.utils import write_jsonl_async
 
 
+def read_json_or_jsonl(file_path: str):
+    if file_path.endswith('.json'):
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    elif file_path.endswith('.jsonl'):
+        with open(file_path, 'r') as file:
+            return [json.loads(line) for line in file]
+    else:
+        raise ValueError("Unsupported file format. Only '.json' and '.jsonl' files are supported.")
+
+async def validate_and_format_question(question: dict, data_source: str) -> Optional[ForecastingQuestion]:
+    for i in range(2):
+        forecasting_question = await question_formater.from_string(
+            question['title'],
+            data_source,
+            question_type=question.get('question_type'),
+            url=question.get('url',None),
+            metadata=question.get('metadata',None),
+            body=question.get('body', None),
+            date=question.get('resolution_date', None)
+        )
+        if await question_formater.validate_question(forecasting_question):
+            break
+        else:
+            print(f"Invalid question: {question}")
+            forecasting_question = None
+            await asyncio.sleep(1)
+
+    return forecasting_question
+
+
 async def process_questions_from_file(file_path: str, data_source: str, max_questions: Optional[int]) -> List[ForecastingQuestion]:
-    with open(file_path, 'r') as file:
-        questions = json.load(file)
+    
+    questions = read_json_or_jsonl(file_path)
     
     max_questions = max_questions if max_questions else len(questions)
     tasks = []
 
     for question in questions[:max_questions]:
-        task = asyncio.create_task(question_formater.from_string(question, data_source))
-        tasks.append(task)
+        tasks.append(validate_and_format_question(question, data_source))
+     
 
     forecasting_questions = await asyncio.gather(*tasks)
-    return forecasting_questions
+    count_none = forecasting_questions.count(None)
+    forecasting_questions = [fq for fq in forecasting_questions if fq is not None]
+    return forecasting_questions, count_none
 
 async def main():
-    file_path = 'data/politics_qs_2.json'
+    file_path = '../scripts/QUESTIONS_CLEANED_MODIFIED.jsonl'
     data_source = 'synthetic'
-    max_questions = 5
+    max_questions = 30
 
-    forecasting_questions = await process_questions_from_file(file_path, data_source, max_questions)
+    forecasting_questions, none_count = await process_questions_from_file(file_path, data_source, max_questions)
+    print(f"Number of invalid questions found: {none_count}")
 
     data_to_write = [fq.dict() for fq in forecasting_questions]
     for data in data_to_write:
         data["id"] = str(data["id"])
         data["resolution_date"] = str(data["resolution_date"])
 
-    await write_jsonl_async('data/politics_qs_2_formated.jsonl', data_to_write, append=False)
+    await write_jsonl_async('data/questions_cleaned_formated.jsonl', data_to_write, append=False)
 
 
 if __name__ == "__main__":
