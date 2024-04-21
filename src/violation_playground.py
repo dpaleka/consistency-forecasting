3332b3522c14083d6817e120c8baf87ae5ed13dc
abhimanyu <abhimanyupallavisudhir@gmail.com>
2024-04-21 17:35:40 +0530

    did a bunch of checkers -- CondChecker is wrong though

--- src/violation_playground.py
+++ src/violation_playground.py
@@ -1,13 +1,19 @@
 from forecasters import BasicForecaster
-from static_checks.Checker import Checker, NegChecker#, AndChecker, OrChecker, AndOrChecker, ButChecker, CondChecker, ConsequenceChecker, ParaphraseChecker, SymmetryAndChecker, SymmetryOrChecker, CondCondChecker
+from static_checks.Checker import Checker, NegChecker, AndChecker, OrChecker, AndOrChecker, ButChecker, CondChecker#, ConsequenceChecker, ParaphraseChecker, SymmetryAndChecker, SymmetryOrChecker, CondCondChecker
 
 #basic_forecaster = BasicForecaster()
 
 neg_checker = NegChecker()
+and_checker = AndChecker()
+or_checker = OrChecker()
+andor_checker = AndOrChecker()
+but_checker = ButChecker()
+cond_checker = CondChecker()
 
-v=neg_checker.max_min_arbitrage({
+v=cond_checker.max_min_arbitrage({
     "P": 0.5,
-    "not_P": 0.3
+    "Q_given_P" : 0.3,
+    "P_and_Q" : 0.2
 })
 
 print(v)
