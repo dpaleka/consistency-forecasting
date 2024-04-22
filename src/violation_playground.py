e140560e477d1a42f4d9655b75e7edcd35c9c68d
abhimanyu <abhimanyupallavisudhir@gmail.com>
2024-04-22 09:25:39 +0100

    something must be wrong -- it doesn't try to maximize at all when I initialize arbitrageur_answers_list_initial to answers?

--- src/violation_playground.py
+++ src/violation_playground.py
@@ -20,12 +20,52 @@ but_checker = ButChecker()
 cond_checker = CondChecker()
 cons_checker = ConsequenceChecker()
 
-v = cons_checker.max_min_arbitrage(
-    {
-        "P": 0.5,
-        "cons_P": 0.7,
-    }
-)
+# v = neg_checker.max_min_arbitrage(
+#     {
+#         "P": 0.5,
+#         "not_P": 0.9,
+#     }
+# )
+
+# v = and_checker.max_min_arbitrage(
+#     {
+#         "P": 0.5,
+#         "Q": 0.3,
+#         "P_and_Q": 0.9,
+#     }
+# )
+
+# v = or_checker.max_min_arbitrage(
+#     {
+#         "P": 0.5,
+#         "Q": 0.3,
+#         "P_or_Q": 0.9,
+#     }
+# )
+
+# v = andor_checker.max_min_arbitrage(
+#     {
+#         "P": 0.5,
+#         "Q": 0.3,
+#         "P_and_Q": 0.9,
+#         "P_or_Q": 0.7,
+#     }
+# )
+
+# v = but_checker.max_min_arbitrage(
+#     {
+#         "P": 0.5,
+#         "P_or_Q": 0.3,
+#         "Q_and_not_P": 0.9,
+#     }
+# )
+
+# v = cons_checker.max_min_arbitrage(
+#     {
+#         "P": 0.5,
+#         "cons_P": 0.7,
+#     }
+# )
 
 # wrong
 # v=cond_checker.max_min_arbitrage({
