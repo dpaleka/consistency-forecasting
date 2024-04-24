1e50052a62bdbee109f039654266a11b21c8c7a4
Daniel Paleka <danepale@gmail.com>
2024-04-24 15:18:09 +0200

    move some code and loosen ruff, fix data validation hook

--- src/forecasters/__init__.py
+++ src/forecasters/__init__.py
@@ -1,5 +1,3 @@
-from .basic_forecaster import BasicForecaster
-from .forecaster import Forecaster, Prob, Prob_cot, ForecastingQuestionTuple, ProbsTuple
-from .cot_forecaster import COT_Forecaster
-
-
+from .basic_forecaster import BasicForecaster  # noqa
+from .forecaster import Forecaster, Prob, Prob_cot, ForecastingQuestionTuple, ProbsTuple  # noqa
+from .cot_forecaster import COT_Forecaster  # noqa
