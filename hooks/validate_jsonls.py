88e3b1b14bc848478a975a80b7a97b2ee0c93947
Daniel Paleka <danepale@gmail.com>
2024-05-16 01:57:26 +0200

    Add option to validate all JSONL files even if not in git diff

--- hooks/validate_jsonls.py
+++ hooks/validate_jsonls.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
+import os
+import git
 
 # Define the script to run and the list of JSONL files to validate
 # Intended to be run from the root of the repository, as when running the pre-commit hook
@@ -8,6 +10,8 @@ import sys
 # Need to do this hack to import stuff
 sys.path.append(".")
 from src.common.path_utils import get_data_path, get_src_path
+
+validate_all = os.getenv("VALIDATE_ALL", False)
 
 script = f"{get_src_path()}/validate_fq_jsonl.py"
 jsonl_files = []
@@ -43,7 +47,6 @@ for file in tuple_data_dir.rglob("*.jsonl"):
 print(f"{len(jsonl_files)} jsonl files found")
 
 # now get the git diff ones
-import git
 
 repo = git.Repo(search_parent_directories=True)
 diff = repo.git.diff("HEAD", name_only=True)
@@ -51,12 +54,15 @@ diff_files: list[str] = diff.splitlines()
 diff_files = [f for f in diff_files if f.endswith(".jsonl")]
 
 
-jsonl_files_to_check = []
-for file_path in jsonl_files:
-    for diff_file in diff_files:
-        if file_path["file"].endswith(diff_file):
-            jsonl_files_to_check.append(file_path)
-            break
+if validate_all:
+    jsonl_files_to_check = jsonl_files
+else:
+    jsonl_files_to_check = []
+    for file_path in jsonl_files:
+        for diff_file in diff_files:
+            if file_path["file"].endswith(diff_file):
+                jsonl_files_to_check.append(file_path)
+                break
 
 print(f"{len(jsonl_files_to_check)} jsonl files to check")
 
@@ -76,16 +82,14 @@ def validate_file(file_path):
     return True
 
 
-# Main logic
-if __name__ == "__main__":
-    all_valid = True
-    for file_path in jsonl_files_to_check:
-        print(f"{file_path}")
-        if not validate_file(file_path):
-            all_valid = False
-            break  # Stop on first failure
+all_valid = True
+for file_path in jsonl_files_to_check:
+    print(f"{file_path}")
+    if not validate_file(file_path):
+        all_valid = False
+        break  # Stop on first failure
 
-    if not all_valid:
-        sys.exit(1)  # Exit with a non-zero status code to indicate failure
+if not all_valid:
+    sys.exit(1)  # Exit with a non-zero status code to indicate failure
 
-    print("Validation successful for all files.")
+print("Validation successful for all files.")
