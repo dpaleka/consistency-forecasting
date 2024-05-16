449bb589efaabe725b848e26381ffbcafb0cadb9
abhimanyu <abhimanyupallavisudhir@gmail.com>
2024-05-16 15:39:58 +0100

    recompile tuples

--- src/instantiation.py
+++ src/instantiation.py
@@ -3,11 +3,9 @@ import jsonlines
 import asyncio
 
 
-
-
 # MODEL = "gpt-3.5-turbo"
 MODEL = "gpt-4-turbo"
-#MODEL = 'gpt-4o'
+# MODEL = 'gpt-4o'
 
 # from static_checks.MiniInstantiator import MiniInstantiator
 from static_checks.Checker import (
