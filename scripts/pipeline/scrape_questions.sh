5ff672fdda4f691b1be22e8120125311c966c6c0
amaster97 <amaster1997@gmail.com>
2024-05-16 22:03:09 +0200

    updated pipeline
    some issues with format_questions.py
    Need to fix to generate new set of qustions_cleaned_formatted.jsonl
    
    This should be the correct ommand through according to the flags

--- scripts/pipeline/scrape_questions.sh
+++ scripts/pipeline/scrape_questions.sh
@@ -6,4 +6,4 @@ cp metaculus.json QUESTIONS_CLEANED.json
 python3 reformat_entries.py
 #python3 add_body.py --filename QUESTIONS_CLEANED_MODIFIED.json
 python3 reshape_metaculus.py --filename QUESTIONS_CLEANED_MODIFIED.json
-python3 ../../src/format_questions.py
+python3 ../../src/format_questions.py -f QUESTIONS_CLEANED_MODIFIED.jsonl -d real -o questions_cleaned_formatted.jsonl -m 400 
