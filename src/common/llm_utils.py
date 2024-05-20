b0ce71349a77a59ae8eb8b91f7ce60645710ee66
Daniel Paleka <danepale@gmail.com>
2024-05-20 13:30:29 +0200

    fix multiple issues with verification add model and fill_in_body options to format_and_verify_questions.py; remove verification logging from git; remove chardet

--- src/common/llm_utils.py
+++ src/common/llm_utils.py
@@ -333,11 +333,12 @@ async def answer(
     **kwargs,
 ) -> BaseModel:
     messages = prepare_messages(prompt, preface, examples)
-    options = {
+    default_options = {
         "model": "gpt-4-1106-preview",
         "temperature": 0.0,
         "response_model": PlainText,
-    } | kwargs
+    }
+    options = default_options | kwargs  # override defaults with kwargs
     return await query_api_chat(messages=messages, **options)
 
 
@@ -405,7 +406,7 @@ async def parallelized_call(
     print(f"Running {func} on {len(data)} datapoints")
 
     if os.getenv("SINGLE_THREAD"):
-        return [await func(text=d) for d in data]
+        return [await func(d) for d in data]
 
     sem = asyncio.Semaphore(max_concurrent_queries)
 
