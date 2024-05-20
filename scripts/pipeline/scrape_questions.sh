882b30c7be5e08fd477a7c774c61388a4e210c11
amaster97 <amaster1997@gmail.com>
2024-05-20 17:24:06 +0200

    new questions formatted correctly scraping

--- scripts/pipeline/scrape_questions.sh
+++ scripts/pipeline/scrape_questions.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Call metaculus.py script
-python3 metaculus.py
+python3.12 metaculus.py
 cp metaculus.json QUESTIONS_CLEANED.json
-python3 reformat_entries.py
-#python3 add_body.py --filename QUESTIONS_CLEANED_MODIFIED.json
-python3 reshape_metaculus.py --filename QUESTIONS_CLEANED_MODIFIED.json
-python3 ../../src/format_questions.py -f QUESTIONS_CLEANED_MODIFIED.jsonl -d real -o questions_cleaned_formatted.jsonl -m 400 
+python3.12 reformat_entries.py
+python count_entries.py -f QUESTIONS_CLEANED.json
+python3.12 add_body.py QUESTIONS_CLEANED_MODIFIED.json
+python3.12 reshape_metaculus.py --filename QUESTIONS_CLEANED_MODIFIED.json
+python3.12 ../../src/format_and_verify_questions.py -f QUESTIONS_CLEANED_MODIFIED.jsonl -d real -o questions_cleaned_formatted.jsonl -m 400 
