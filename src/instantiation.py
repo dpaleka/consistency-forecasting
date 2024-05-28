1b65f3da4c5e42360ce113dd73f20e9fd53af3d7
abhimanyu <abhimanyupallavisudhir@gmail.com>
2024-05-29 00:51:14 +0100

    instantiated 10 of each

--- src/instantiation.py
+++ src/instantiation.py
@@ -40,25 +40,25 @@ else:
     TUPLES_PATH: Path = get_data_path() / "tuples_alt_noex/"
 
 checkers: dict[str, Checker] = {
-    # "NegChecker": NegChecker(path=TUPLES_PATH / "NegChecker.jsonl"),
-    # "AndChecker": AndChecker(path=TUPLES_PATH / "AndChecker.jsonl"),
-    # "OrChecker": OrChecker(path=TUPLES_PATH / "OrChecker.jsonl"),
-    # "AndOrChecker": AndOrChecker(path=TUPLES_PATH / "AndOrChecker.jsonl"),
-    # "ButChecker": ButChecker(path=TUPLES_PATH / "ButChecker.jsonl"),
-    # "CondChecker": CondChecker(path=TUPLES_PATH / "CondChecker.jsonl"),
+    "NegChecker": NegChecker(path=TUPLES_PATH / "NegChecker.jsonl"),
+    "AndChecker": AndChecker(path=TUPLES_PATH / "AndChecker.jsonl"),
+    "OrChecker": OrChecker(path=TUPLES_PATH / "OrChecker.jsonl"),
+    "AndOrChecker": AndOrChecker(path=TUPLES_PATH / "AndOrChecker.jsonl"),
+    "ButChecker": ButChecker(path=TUPLES_PATH / "ButChecker.jsonl"),
+    "CondChecker": CondChecker(path=TUPLES_PATH / "CondChecker.jsonl"),
     "ConsequenceChecker": ConsequenceChecker(
         path=TUPLES_PATH / "ConsequenceChecker.jsonl"
     ),
-    # "ParaphraseChecker": ParaphraseChecker(
-    #     path=TUPLES_PATH / "ParaphraseChecker.jsonl"
-    # ),
-    # "SymmetryAndChecker": SymmetryAndChecker(
-    #     path=TUPLES_PATH / "SymmetryAndChecker.jsonl"
-    # ),
-    # "SymmetryOrChecker": SymmetryOrChecker(
-    #     path=TUPLES_PATH / "SymmetryOrChecker.jsonl"
-    # ),
-    # "CondCondChecker": CondCondChecker(path=TUPLES_PATH / "CondCondChecker.jsonl"),
+    "ParaphraseChecker": ParaphraseChecker(
+        path=TUPLES_PATH / "ParaphraseChecker.jsonl"
+    ),
+    "SymmetryAndChecker": SymmetryAndChecker(
+        path=TUPLES_PATH / "SymmetryAndChecker.jsonl"
+    ),
+    "SymmetryOrChecker": SymmetryOrChecker(
+        path=TUPLES_PATH / "SymmetryOrChecker.jsonl"
+    ),
+    "CondCondChecker": CondCondChecker(path=TUPLES_PATH / "CondCondChecker.jsonl"),
 }
 
 
