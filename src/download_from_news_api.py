96fc4f8728015e7ccc8dde0896f7946ca1a17d7c
FlightVin <vineeth.bhat@gmail.com>
2024-07-13 17:07:04 +0530

    Add script to download data from News API
    Add a script that has functionality to download data from News API and
    handle articles as paginated results.

---
+++ src/download_from_news_api.py
@@ -0,0 +1,191 @@
+import requests
+import os
+import math
+import json
+from dotenv import load_dotenv
+from simple_parsing import ArgumentParser
+from datetime import datetime
+
+load_dotenv()
+
+# Constants
+NEWS_API_DATA_DUMP_DIR = "./data/news_feed_fq_generation/news_feed_data"
+NEWS_API_DOMAINS = [
+    "apnews.com",
+    "bloomberg.com",
+    "reuters.com",
+]
+NEWS_API_NUM_ARTICLES_PER_PAGE = 100  # maximum allowed value
+NEWS_API_DEFAULT_PARAMS = {
+    "pageSize": NEWS_API_NUM_ARTICLES_PER_PAGE,
+    "language": "en",
+    "sortBy": "popularity",
+    "domains": ",".join(NEWS_API_DOMAINS),
+}
+NEWS_API_ENDPOINT = "v2/everything"
+NEWS_API_REQUEST_URL = f"https://newsapi.org/{NEWS_API_ENDPOINT}"
+
+
+def parse_date(date_str: str) -> datetime:
+    """
+    Given a date in the format YYYY-MM-DD, returns the corresponding datestring.
+    Has validation for the correct type.
+
+    :date_str: The date as a string
+
+    :returns: Given date as a datetime object
+    """
+    try:
+        return datetime.strptime(date_str, "%Y-%m-%d").date()
+    except ValueError:
+        raise TypeError(
+            f"Invalid date format: {date_str}. Date must be in YYYY-MM-DD format."
+        )
+
+
+def get_news_from_api(
+    start_date: datetime, end_date: datetime, page_number: int, api_key: str
+) -> dict:
+    """
+    Downloads popular news at the given page number from news API
+
+    :start_date: Start date for downloading news
+    :end_date: End date for downloading news
+    :page_number: The page number to be downloaded
+    :api_key: News API key
+
+    :returns: dict containing the response
+    """
+    current_params = NEWS_API_DEFAULT_PARAMS.copy()
+    current_params["from"] = start_date.strftime("%Y-%m-%d")
+    current_params["to"] = end_date.strftime("%Y-%m-%d")
+    current_params["page"] = page_number
+    current_params["apiKey"] = api_key
+
+    response = requests.get(NEWS_API_REQUEST_URL, params=current_params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise RuntimeError(
+            f"Failed to retrieve data: {response.status_code}.\n\tError response Dict:\n\t\t{response.__dict__}"
+        )
+
+
+def news_api_download_file_path(
+    start_date: datetime, end_date: datetime, num_pages: int
+) -> str:
+    """
+    File path to download the articles from News API
+
+    :start_date: Start date for downloading news
+    :end_date: End date for downloading news
+    :num_pages: Number of pages (each containing max 100 articles) to be downloaded
+
+    :returns: file path where the news will be downloaded
+    """
+    if num_pages == -1:
+        news_save_file_name = f"news_api_from_{start_date.strftime('%Y-%m-%d')}_to_{end_date.strftime('%Y-%m-%d')}_num_pages_all.jsonl"
+    else:
+        news_save_file_name = f"news_api_from_{start_date.strftime('%Y-%m-%d')}_to_{end_date.strftime('%Y-%m-%d')}_num_pages_{num_pages}.jsonl"
+
+    return os.path.join(NEWS_API_DATA_DUMP_DIR, news_save_file_name)
+
+
+def download_news_from_api(
+    start_date: datetime, end_date: datetime, num_pages: int, api_key: str
+) -> None:
+    """
+    Downloads popular news as response pages from News API from a set of curated domains
+
+    :start_date: Start date for downloading news
+    :end_date: End date for downloading news
+    :num_pages: Number of pages (each containing max 100 articles) to be downloaded
+    :api_key: News API key
+
+    :returns: None
+    """
+    articles_download_path = news_api_download_file_path(
+        start_date, end_date, num_pages
+    )
+
+    # Failsafe to prevent redundant News API queries
+    if os.path.exists(articles_download_path):
+        print(
+            f"\nThe data has already been downloaded to {articles_download_path} \nReturning without downloading anything new!"
+        )
+        return
+
+    if not os.getenv("NEWS_API_KEY"):
+        raise RuntimeError("OS enviroment for NEWS_API_KEY either missing or None!")
+
+    # Make the data dump directory
+    os.makedirs(NEWS_API_DATA_DUMP_DIR, exist_ok=True)
+
+    first_page_news = get_news_from_api(start_date, end_date, 1, api_key)
+
+    # Calculating number of pages requried
+    total_number_of_pages = math.ceil(
+        first_page_news["totalResults"] / NEWS_API_NUM_ARTICLES_PER_PAGE
+    )
+    num_pages = (
+        total_number_of_pages
+        if num_pages == -1 or num_pages > total_number_of_pages
+        else num_pages
+    )
+
+    if num_pages > 1:
+        print(
+            f"\nIf you have a developer account API, you will not be able to access {num_pages} (>1) pages!\n"
+        )
+
+    print(
+        f"Saving {num_pages} from the total available {total_number_of_pages} pages of"
+        + f" news artilces with {NEWS_API_NUM_ARTICLES_PER_PAGE} articles each to {articles_download_path}"
+    )
+
+    # Saving the pages
+    for page_number in range(1, num_pages + 1):
+        if page_number == 1:
+            news_articles = first_page_news["articles"]
+        else:
+            news_articles = get_news_from_api(
+                start_date, end_date, page_number, api_key
+            )["articles"]
+        with open(articles_download_path, "a") as jsonl_file:
+            for article in news_articles:
+                jsonl_file.write(json.dumps(article) + "\n")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--start-date",
+        type=parse_date,
+        help="Start date for downloading news in YYYY-MM-DD format",
+        required=True,
+    )
+    parser.add_argument(
+        "--end-date",
+        type=parse_date,
+        help="End date for downloading news in YYYY-MM-DD format",
+        required=True,
+    )
+    parser.add_argument(
+        "--num-pages",
+        type=int,
+        help="""
+        News API returns data in a paginated form. We set the number of articles downloaded per page to a 100 (maximum),
+        By default, we only download the first page. 
+
+        Set to the number of pages to be downloaded. Set to -1 to download all pages.
+        """,
+        default=1,
+    )
+
+    args = parser.parse_args()
+
+    assert args.num_pages == -1 or args.num_pages > 0
+
+    download_news_from_api(
+        args.start_date, args.end_date, args.num_pages, os.getenv("NEWS_API_KEY")
+    )
