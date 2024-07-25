add recalculate argument to reevaluation, fix plots a bit, lower precision of viols

src/reevaluation.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 from pathlib import Path
 from typing import Any
 from static_checks.Checker import (
@@ -17,6 +18,7 @@ from common.path_utils import get_data_path
 from common.utils import round_floats
 import plotnine as p9
 import pandas as pd
+import numpy as np
 
 TUPLES_PATH: Path = get_data_path() / "tuples/"
 FORECASTS_PATH: Path = get_data_path() / "forecasts"
@@ -116,7 +118,7 @@ def append_violations(
     if write:
         with open(tuples_files, "w", encoding="utf-8") as f:
             for tuple in tuples:
-                tuple = round_floats(tuple, precision=7)
+                tuple = round_floats(tuple, precision=5)
                 f.write(json.dumps(tuple) + "\n")
     return viols_all, checks_all
 
@@ -227,17 +229,19 @@ def plot(viols: list[float], binwidth=0.005, cap=None):
     """Plot a histogram of violations."""
     df = pd.DataFrame({"violation": viols})
     if cap:
-        df["viol_capped"] = df["violation"].apply(lambda x: min(x, cap))
+        df["Violation"] = df["violation"].apply(lambda x: min(x, cap))
     else:
-        cap = 999999
+        cap = max(viols)
+
+    # Create custom breaks and labels
+    breaks = np.arange(0, cap + binwidth, cap / 5)
+    labels = [f"{b:.2f}" for b in breaks[:-1]] + [f"> {cap:.2f}"]
+
     return (
-        p9.ggplot(df, p9.aes(x="viol_capped"))
-        # + p9.geom_histogram(binwidth=binwidth, closed='left')
+        p9.ggplot(df, p9.aes(x="Violation"))
         + p9.geom_density()
-        # + p9.scale_x_continuous(
-        #     #breaks=range(int(df['CappedValues'].min()), int(cap) + 1),
-        #     labels=lambda l: ["%.1f" % v if v != cap else ">{cap}" for v in l])
-        # + p9.theme_minimal()
+        + p9.scale_x_continuous(breaks=breaks, labels=labels)
+        + p9.ylab("Density")
     )
 
 
@@ -248,16 +252,19 @@ def plot_all(checker_viols, metric="default", binwidth=0.005, cap=None, ymax=30)
     for checker, viols in checker_viols.items():
         df = df._append(pd.DataFrame({"violation": viols[metric], "checker": checker}))
     if not cap:
-        cap = 999999
-    df["viol_capped"] = df["violation"].apply(lambda x: min(x, cap))
+        cap = df["violation"].max()
+    df["Violation"] = df["violation"].apply(lambda x: min(x, cap))
+
+    # Create custom breaks and labels
+    breaks = np.arange(0, cap + binwidth, cap / 5)
+    labels = [f"{b:.2f}" for b in breaks[:-1]] + [f"> {cap:.2f}"]
+
     return (
-        p9.ggplot(df, p9.aes(x="viol_capped", color="checker"))
+        p9.ggplot(df, p9.aes(x="Violation", color="checker"))
         + p9.geom_density()
         + p9.ylim(0, ymax)
-        # + p9.scale_x_continuous(
-        #     #breaks=range(int(df['CappedValues'].min()), int(cap) + 1),
-        #     labels=lambda l: ["%.1f" % v if v != cap else ">{cap}" for v in l])
-        # + p9.theme_minimal()
+        + p9.scale_x_continuous(breaks=breaks, labels=labels)
+        + p9.ylab("Density")
     )
 
 
@@ -270,7 +277,7 @@ def get_stats_from_paths(paths, metrics=None, write: Path | None = None):
         if isinstance(write, str):
             write = FORECASTS_PATH / write
         with open(write, "w", encoding="utf-8") as f:
-            stats = round_floats(stats, precision=7)
+            stats = round_floats(stats, precision=4)
             f.write(json.dumps(stats, indent=4))
 
 
@@ -293,13 +300,33 @@ paths_cf_gpt_4omini_sample = [
 paths = paths_adv + paths_gpt_3_5 + paths_gpt_4o
 metrics = ["default", "frequentist"]
 
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process violations for checkers.")
+    parser.add_argument(
+        "-r", "--recalculate", action="store_true", help="Recalculate violations"
+    )
+    args = parser.parse_args()
+
+    RECALC: bool = args.recalculate
+
+    print(f"Recalculating violations: {RECALC}")
+
+    # Ask for confirmation before modifying files
+    if RECALC:
+        confirm = input(
+            "This will modify existing files and take some time to run. Are you sure you want to continue? (y/[n]): "
+        )
+        if confirm.lower() != "y":
+            print("Operation cancelled.")
+            exit()
+
     # TODO add some notifications about what files will get modified, and y/n. ideally together with the edits that introduces cli args to this
-    append_violations_all(paths_adv, metrics=metrics, recalc=True, write=True)
-    append_violations_all(paths_gpt_3_5, metrics=metrics, recalc=True, write=True)
-    append_violations_all(paths_gpt_4o, metrics=metrics, recalc=True, write=True)
+    append_violations_all(paths_adv, metrics=metrics, recalc=RECALC, write=True)
+    append_violations_all(paths_gpt_3_5, metrics=metrics, recalc=RECALC, write=True)
+    append_violations_all(paths_gpt_4o, metrics=metrics, recalc=RECALC, write=True)
     append_violations_all(
-        paths_cf_gpt_4omini_sample, metrics=metrics, recalc=True, write=True
+        paths_cf_gpt_4omini_sample, metrics=metrics, recalc=RECALC, write=True
     )
     print(get_stats_from_paths(paths_adv, metrics=metrics, write="stats_adv.json"))
     print(
@@ -315,9 +342,27 @@ if __name__ == "__main__":
             write="stats_cf_gpt_4omini_sample.json",
         )
     )
-    # checker_viols, checker_checks = append_violations_all(
-    #     paths_adv, metrics=metrics, recalc=False, write=False
-    # )
-    # print(plot(checker_viols["CondChecker"]["default"], cap=0.1))
-    # print(plot_all(checker_viols, metric="default", cap=0.1))
-    # print(plot_all(checker_viols, metric="frequentist", cap=1.0, ymax=10))
+    checker_viols, checker_checks = append_violations_all(
+        paths_adv, metrics=metrics, recalc=RECALC, write=False
+    )
+    print(plot(checker_viols["CondChecker"]["default"], cap=0.1))
+    print(plot_all(checker_viols, metric="default", cap=0.1))
+    print(plot_all(checker_viols, metric="frequentist", cap=1.0, ymax=10))
+
+    # save the plots
+    plot(checker_viols["CondChecker"]["default"], cap=0.1).save(
+        "cond_checker_default.png"
+    )
+
+    # remove AndChecker from the plot bc it looks ugly
+    checker_viols.pop("AndChecker")
+    # remove ConsequenceChecker from the plot bc the data might not be correct
+    checker_viols.pop("ConsequenceChecker")
+    keys_to_rename = [k for k in checker_viols.keys() if k.endswith("Checker")]
+    for k in keys_to_rename:
+        checker_viols[k.replace("Checker", "")] = checker_viols.pop(k)
+
+    plot_all(checker_viols, metric="default", cap=0.1).save("all_default.png")
+    plot_all(checker_viols, metric="frequentist", cap=1.0, ymax=10).save(
+        "all_frequentist.png"
+    )