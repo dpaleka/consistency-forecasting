4405d6372ed33f3690c0b011a3cf85796c1daaf8
abhimanyu <abhimanyupallavisudhir@gmail.com>
2024-09-04 14:50:01 +0100

    cleanup

--- src/static_checks/MiniInstantiator.py
+++ src/static_checks/MiniInstantiator.py
@@ -201,13 +201,14 @@ class MiniInstantiator(ABC):
         **kwargs,
     ) -> Union["Self.OutputFormat", List["Self.OutputFormat"]]:
         if verify_before_instantion:
+            print("Base sentences:", base_sentences)
+            print(f"Instantiating with instantiator {self.__class__.__name__}")
             for i in range(n_verification):
-                print("Base sentences:", base_sentences)
                 print(f"Instantiation attempt {i}")
                 output = self._instantiate_sync(base_sentences, **kwargs)
                 based_sentences = self.to_base_sentence_format_stripped(base_sentences)
                 print(f"Verifying output: {output}")
-                verification = self.verify_sync(output, based_sentences)
+                verification = self.verify_sync(output, based_sentences, **kwargs)
                 print("Verification result:", verification)
                 if verification.valid:
                     return output
@@ -241,14 +242,14 @@ class MiniInstantiator(ABC):
         **kwargs,
     ) -> Union["Self.OutputFormat", List["Self.OutputFormat"]]:
         if verify_before_instantion:
+            print("Base sentences:", base_sentences)
+            print(f"Instantiating with instantiator {self.__class__.__name__}")
             for i in range(n_verification):
-                print("Base sentences:", base_sentences)
-                print(f"Instantiating with instantiator {self.__class__.__name__}")
                 print(f"Instantiation attempt {i}")
                 output = await self._instantiate(base_sentences, **kwargs)
                 based_sentences = self.to_base_sentence_format_stripped(base_sentences)
                 print(f"Verifying output: {output}")
-                verification = await self.verify(output, based_sentences)
+                verification = await self.verify(output, based_sentences, **kwargs)
                 print("Verification result:", verification)
                 if verification.valid:
                     return output
