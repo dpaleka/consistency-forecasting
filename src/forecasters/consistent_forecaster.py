3c688b0bd3396fbde53aaa085c8f6f9449f5cc7a
abhimanyupallavisudhir <abhimanyupallavisudhir@gmail.com>
2024-09-19 22:35:47 +0000

    trying to make cfcaster support multiple checks

--- src/forecasters/consistent_forecaster.py
+++ src/forecasters/consistent_forecaster.py
@@ -10,10 +10,6 @@ from static_checks import (
     ButChecker,
     CondChecker,
 )
-from static_checks.tuple_relevance import (
-    get_relevant_questions,
-    get_relevant_questions_sync,
-)
 from common.path_utils import get_data_path
 
 
@@ -78,130 +74,6 @@ class ConsistentForecaster(Forecaster):
         self.bq_func_kwargs["simulate"] = self.kwargs.get("simulate", False)
         self.instantiation_kwargs["cost_log"] = self.kwargs.get("cost_log", None)
         self.instantiation_kwargs["simulate"] = self.kwargs.get("simulate", False)
-
-    def bq_function(
-        self,
-        sentence: ForecastingQuestion,
-        keys: list = None,
-        n_relevance: int = 10,
-        n_return: int = 1,
-        tuple_size=2,
-        base_data_path=None,
-        **kwargs,
-    ) -> dict[str, ForecastingQuestion]:
-        """Get relevant questions for the given sentence.
-
-        Args:
-
-        sentence (ForecastingQuestion): Sentence to get relevant questions for.
-        keys (list): Keys to use for the relevant questions.
-        n_relevance (int): Number of retrieved questions to consider.
-        n_return (int): Number of questions to return.
-        tuple_size (int): Size of the tuple to get.
-        base_data_path (Path): Path to questions to retrieve from.
-
-        """
-        if keys is None:
-            keys = ["P", "Q", "R", "S", "T"]
-        if base_data_path is None:
-            base_data_path = self.base_data_path
-        tup = get_relevant_questions_sync(
-            existing_questions=[sentence],
-            n_relevance=n_relevance,
-            n_return=n_return,
-            tuple_size=tuple_size,
-            base_data_path=base_data_path,
-            **kwargs,
-        )[0][0]
-        return {k: fq for k, fq in zip(keys, tup)}
-
-    async def bq_function_async(
-        self,
-        sentence: ForecastingQuestion,
-        keys: list = None,
-        n_relevance: int = 10,
-        n_return: int = 1,
-        tuple_size=2,
-        base_data_path=None,
-        **kwargs,
-    ) -> dict[str, ForecastingQuestion]:
-        """Get relevant questions for the given sentence.
-
-        Args:
-
-        sentence (ForecastingQuestion): Sentence to get relevant questions for.
-        keys (list): Keys to use for the relevant questions.
-        n_relevance (int): Number of retrieved questions to consider.
-        n_return (int): Number of questions to return.
-        tuple_size (int): Size of the tuple to get.
-        base_data_path (Path): Path to questions to retrieve from.
-
-        """
-        if keys is None:
-            keys = ["P", "Q", "R", "S", "T"]
-        if base_data_path is None:
-            base_data_path = self.base_data_path
-        res = await get_relevant_questions(
-            existing_questions=[sentence],
-            n_relevance=n_relevance,
-            n_return=n_return,
-            tuple_size=tuple_size,
-            base_data_path=base_data_path,
-            **kwargs,
-        )
-        tup = res[0][0]
-        return {k: fq for k, fq in zip(keys, tup)}
-
-    def instantiate_cons_tuples(
-        self,
-        sentence: ForecastingQuestion,
-        bq_func_kwargs: dict = None,
-        instantiation_kwargs: dict = None,
-        **kwargs,
-    ) -> list[dict[str, ForecastingQuestion]]:
-        """Instantiate tuples for arbitraging the given sentence.
-
-        Args:
-            sentence (ForecastingQuestion): Sentence to instantiate consistent tuples for.
-            bq_func_kwargs (dict): Keyword arguments for bq_function.
-            instantiation_kwargs (dict): Keyword arguments for instantiation.
-
-        """
-        if self.coerce_nonbinary_qs and not sentence.question_type == "binary":
-            sentence.question_type = "binary"
-        kwargs = self.kwargs | (kwargs or {})
-        bq_func_kwargs = self.bq_func_kwargs | (bq_func_kwargs or {})
-        instantiation_kwargs = self.instantiation_kwargs | (instantiation_kwargs or {})
-        bq_func_kwargs["cost_log"] = kwargs.get("cost_log", None)
-        bq_func_kwargs["simulate"] = kwargs.get("simulate", False)
-        instantiation_kwargs["cost_log"] = kwargs.get("cost_log", None)
-        instantiation_kwargs["simulate"] = kwargs.get("simulate", False)
-
-        # pre-generate bq_tuple for tuple_size=max(check.num_base_questions for check in self.checks)
-        max_tuple_size = max(check.num_base_questions for check in self.checks)
-        bq_tuple_max = self.bq_function(
-            sentence, tuple_size=max_tuple_size, **bq_func_kwargs
-        )
-
-        cons_tuples = []
-        for check in self.checks:
-            bq_tuple = {
-                k: v
-                for k, v in bq_tuple_max.items()
-                if k in list(bq_tuple_max.keys())[: check.num_base_questions]
-            }
-            print(f"{bq_tuple=}")
-            cons_tuple = check.instantiate_sync(bq_tuple, **instantiation_kwargs)
-            print(f"{cons_tuple=}")
-            if isinstance(cons_tuple, list):
-                if len(cons_tuple) == 0:
-                    print(
-                        f"Found no valid instantiated cons_tuple for {check.__class__.__name__}"
-                    )
-                    continue
-                cons_tuple = cons_tuple[0]
-            cons_tuples.append(cons_tuple)
-        return cons_tuples
 
     async def instantiate_cons_tuples_async(
         self,
@@ -283,15 +155,17 @@ class ConsistentForecaster(Forecaster):
             },
             model="gpt-4o",
         )
+        ```
 
         """
-        metadata = {}
+        metadata = []
         ans_P = self.hypocrite.call(sentence, **kwargs)
-        metadata["P"] = (
-            dict(sentence)
-            | {"elicited_prob": ans_P.prob}
-            | {"elicitation_metadata": ans_P.metadata}
-        )
+        metadata_entry = {
+            "name": "P",
+            "elicited_prob": ans_P.prob,
+            "elicitation_metadata": ans_P.metadata,
+        }
+        metadata.append(metadata_entry)
         ans_P = ans_P.prob  ###
 
         cons_tuples = self.instantiate_cons_tuples(
@@ -307,12 +181,18 @@ class ConsistentForecaster(Forecaster):
             del cons_tuple["P"]
             hypocrite_answers = self.hypocrite.elicit(cons_tuple, **kwargs)
 
-            metadata[check.__class__.__name__] = {
-                k: cons_tuple[k].model_dump()
-                | {"elicited_prob": hypocrite_answers[k].prob}
-                | {"elicitation_metadata": hypocrite_answers[k].metadata}
-                for k in cons_tuple
+            metadata_entry = {
+                "name": check.__class__.__name__,
+                "data": {
+                    k: {
+                        **cons_tuple[k].model_dump(),
+                        "elicited_prob": hypocrite_answers[k].prob,
+                        "elicitation_metadata": hypocrite_answers[k].metadata,
+                    }
+                    for k in cons_tuple
+                },
             }
+            metadata.append(metadata_entry)
 
             hypocrite_answers = {k: v.prob for k, v in hypocrite_answers.items()}
 
@@ -359,15 +239,17 @@ class ConsistentForecaster(Forecaster):
             },
             model="gpt-4o",
         )
+        ```
 
         """
-        metadata = {}
+        metadata = []
         ans_P = await self.hypocrite.call_async(sentence, **kwargs)
-        metadata["P"] = (
-            dict(sentence)
-            | {"elicited_prob": ans_P.prob}
-            | {"elicitation_metadata": ans_P.metadata}
-        )
+        metadata_entry = {
+            "name": "P",
+            "elicited_prob": ans_P.prob,
+            "elicitation_metadata": ans_P.metadata,
+        }
+        metadata.append(metadata_entry)
         ans_P = ans_P.prob  ###
 
         cons_tuples = await self.instantiate_cons_tuples_async(
@@ -383,12 +265,19 @@ class ConsistentForecaster(Forecaster):
             del cons_tuple["P"]
             hypocrite_answers = await self.hypocrite.elicit_async(cons_tuple, **kwargs)
 
-            metadata[check.__class__.__name__] = {
-                k: cons_tuple[k].model_dump()
-                | {"elicited_prob": hypocrite_answers[k].prob}
-                | {"elicitation_metadata": hypocrite_answers[k].metadata}
-                for k in cons_tuple
+            metadata_entry = {
+                "name": check.__class__.__name__,
+                "data": {
+                    k: {
+                        **cons_tuple[k].model_dump(),
+                        "elicited_prob": hypocrite_answers[k].prob,
+                        "elicitation_metadata": hypocrite_answers[k].metadata,
+                    }
+                    for k in cons_tuple
+                },
             }
+            metadata.append(metadata_entry)
+
             hypocrite_answers = {k: v.prob for k, v in hypocrite_answers.items()}
 
             hypocrite_answers["P"] = ans_P
