5ed0d91ddbc9d9984a92f8f4a48c21fd503b28aa
abhimanyupallavisudhir <abhimanyupallavisudhir@gmail.com>
2024-09-26 23:03:26 +0000

    update runs

--- see_jobs.sh
+++ see_jobs.sh
@@ -1,8 +1,8 @@
 # each is done once it hits 2001 and also have all the stats files in them
 # # DIR="src/data/forecasts/ConsistentForecaster_N4_tuples_scraped" # DONE
 # # DIR="src/data/forecasts/ConsistentForecaster_N4_tuples_newsapi" # DONE
-DIR="src/data/forecasts/ConsistentForecaster_P4_tuples_scraped" # last: 1951
-# DIR="src/data/forecasts/ConsistentForecaster_P4_tuples_newsapi" # last: 1001
+# # DIR="src/data/forecasts/ConsistentForecaster_P4_tuples_scraped" # DONE
+# DIR="src/data/forecasts/ConsistentForecaster_P4_tuples_newsapi" # last: 1151
 # DIR="src/data/forecasts/ConsistentForecaster_NP4_tuples_scraped" # NOT STARTED
 # DIR="src/data/forecasts/ConsistentForecaster_NP4_tuples_newsapi" # NOT STARTED
 
