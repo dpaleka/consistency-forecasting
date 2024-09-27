caa5cc84e0a49d236c98f2b2e26164e5afe6ef5b
abhimanyupallavisudhir <abhimanyupallavisudhir@gmail.com>
2024-09-27 03:01:02 +0000

    .

--- see_jobs.sh
+++ see_jobs.sh
@@ -2,10 +2,10 @@
 # # DIR="src/data/forecasts/ConsistentForecaster_N4_tuples_scraped" # DONE
 # # DIR="src/data/forecasts/ConsistentForecaster_N4_tuples_newsapi" # DONE
 # # DIR="src/data/forecasts/ConsistentForecaster_P4_tuples_scraped" # DONE
-# # DIR="src/data/forecasts/ConsistentForecaster_P4_tuples_newsapi" # last: 1651 # paused, rerunning
-DIR="src/data/forecasts/ConsistentForecaster_P4_tuples_newsapi_rerun" # last: 0
+# # DIR="src/data/forecasts/ConsistentForecaster_P4_tuples_newsapi" # last: 1901 # paused, rerunning
+# DIR="src/data/forecasts/ConsistentForecaster_P4_tuples_newsapi_rerun" # last: 1
 # DIR="src/data/forecasts/ConsistentForecaster_NP4_tuples_scraped" # last: 201
-# DIR="src/data/forecasts/ConsistentForecaster_NP4_tuples_newsapi" # last: 0
+# DIR="src/data/forecasts/ConsistentForecaster_NP4_tuples_newsapi" # last: 1
 
 for f in $DIR/*.jsonl; do
     stat "$f"
