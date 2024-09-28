1c638a420bfe10573967ea85cb4f9304e5bf6e37
abhimanyupallavisudhir <abhimanyupallavisudhir@gmail.com>
2024-09-28 18:28:07 +0000

    hopefully stuff didn't get deleted

--- see_jobs.sh
+++ see_jobs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # each is done once it hits 2001 and also have all the stats files in them
-# # DIR="src/data/forecasts/ConsistentForecaster_N4_tuples_scraped" # DONE
-# # DIR="src/data/forecasts/ConsistentForecaster_N4_tuples_newsapi" # DONE
-# # DIR="src/data/forecasts/ConsistentForecaster_P4_tuples_scraped" # DONE
-# # DIR="src/data/forecasts/ConsistentForecaster_P4_tuples_newsapi" # DONE
-# # DIR="src/data/forecasts/ConsistentForecaster_NP4_tuples_scraped" # DONE
-# DIR="src/data/forecasts/ConsistentForecaster_NP4_tuples_newsapi" # DONE
+# DIR="src/data/forecasts/ConsistentForecaster_N4_tuples_scraped" # DONE
+# DIR="src/data/forecasts/ConsistentForecaster_N4_tuples_newsapi" # DONE
+# DIR="src/data/forecasts/ConsistentForecaster_P4_tuples_scraped" # DONE
+# DIR="src/data/forecasts/ConsistentForecaster_P4_tuples_newsapi" # DONE
+# DIR="src/data/forecasts/ConsistentForecaster_NP4_tuples_scraped" # DONE
+DIR="src/data/forecasts/ConsistentForecaster_NP4_tuples_newsapi" # DONE
 # DIR="src/data/forecasts/ConsistentForecaster_4xEE1_tuples_synthetic"
 # DIR="src/data/forecasts/ConsistentForecaster_N4_tuples_synthetic"
 # DIR="src/data/forecasts/ConsistentForecaster_P4_tuples_synthetic"
